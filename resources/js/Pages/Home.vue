755c6b79e0f373aaaa3418cffeb436b7b64eecfd
James <james@jastewart.co.uk>
2022-10-04 13:37:49 +0100

    Add pet availability list

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -29,7 +29,38 @@ defineProps({
     >
         <NavBar/>
 
-        <p>JAMES</p>
+        <!-- foreach petCabins, list date and available -->
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                <h1 class="text-4xl text-gray-600 dark:text-gray-200">Northlink Trip Availability</h1>
+            </div>
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6">
+                        <div class="flex items-center">
+                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
+                                <h1 class="text-2xl text-gray-600 dark:text-gray-200">Lerwick to Aberdeen Pet Cabins</h1>
+                                <div class="mt-2 text-gray-500 dark:text-gray-400 text-sm">
+                                    <ul>
+                                        <li v-for="petCabin in petCabins" :key="petCabin.id">
+                                            <span class="text-gray-600 dark:text-gray-200 mr-4">{{ petCabin.date }}</span>
+                                            <span class="text-gray-600 dark:text-gray-200" v-if="petCabin.available">
+                                                ✅
+                                            </span>
+                                            <span class="text-gray-600 dark:text-gray-200" v-else>
+                                                ⛔️
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <p>JAMES</p> -->
     </div>
 </template>
 
