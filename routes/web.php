44c0a8cbed2366aed0fda911e3cabe6732d466ce
James <james@jastewart.co.uk>
2023-01-01 19:12:32 +0000

    Delete it

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/pets', function () {
     $job = JobRun::where('job_name', 'ScrapeOnePaxData')->first();
 
     $payload = [
-        'lastFetched' => isset($job->finished_at->diffForHumans()) ? $job->finished_at->diffForHumans() : null,
+        'lastFetched' => null,
         'currentlyRunning' => $job ? $job->currentlyRunning : false,
     ];
 
@@ -36,7 +36,7 @@ Route::get('/cars', function () {
     $job = JobRun::where('job_name', 'ScrapeCarDataOnePax')->first();
 
     $payload = [
-        'lastFetched' => isset($job->finished_at->diffForHumans()) ? $job->finished_at->diffForHumans() : null,
+        'lastFetched' => null,
         'currentlyRunning' => $job ? $job->currentlyRunning : false,
     ];
 
@@ -62,7 +62,7 @@ Route::get('/cars', function () {
     $job = JobRun::where('job_name', 'ScrapeCarDataOnePax')->first();
 
     $payload = [
-        'lastFetched' => isset($job->finished_at->diffForHumans()) ? $job->finished_at->diffForHumans() : null,
+        'lastFetched' => null,
         'currentlyRunning' => $job ? $job->currentlyRunning : null,
     ];
 
@@ -75,7 +75,7 @@ Route::get('/rooms', function () {
     $job = JobRun::where('job_name', 'GetTripAccommodation')->first();
 
     $payload = [
-        'lastFetched' => isset($job->finished_at->diffForHumans()) ? $job->finished_at->diffForHumans() : null,
+        'lastFetched' => null,
         'currentlyRunning' => $job ? $job->currentlyRunning : null,
     ];
 
