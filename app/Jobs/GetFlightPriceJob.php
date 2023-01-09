cba01268a3acd1301c7b3f4b9fbc2a001120d613
James <james@jastewart.co.uk>
2023-01-09 19:29:09 +0000

    Set user agent

--- app/Jobs/GetFlightPriceJob.php
+++ app/Jobs/GetFlightPriceJob.php
@@ -35,6 +35,9 @@ class GetFlightPriceJob implements ShouldQueue
             'headless' => true,
         ]);
 
+        $ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36";
+        $browser->setUserAgent($ua);
+
         $page = $browser->newPage();
         $page->goto('https://www.loganair.co.uk/');
 
