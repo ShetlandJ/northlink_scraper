92f0daa7b0ffcf71d7c5a7154de43150bd44d343
James <electionnightspecial@gmail.com>
2024-01-10 17:40:18 +0000

    Remove comments

--- resources/js/components/AvailabilityCalendar.vue
+++ resources/js/components/AvailabilityCalendar.vue
@@ -77,14 +77,8 @@ const month = today.getMonth() + 1;
 const year = today.getFullYear();
 
 const routesList = [
-    // { code: "ABKI", name: "Aberdeen to Kirkwall" },
     { code: "ABLE", name: "Aberdeen to Lerwick" },
-    // { code: "KIAB", name: "Kirkwall to Aberdeen" },
-    // { code: "KILE", name: "Kirkwall to Lerwick" },
     { code: "LEAB", name: "Lerwick to Aberdeen" },
-    // { code: "LEKI", name: "Lerwick to Kirkwall" },
-    // { code: "SCST", name: "Scrabster to Stromness" },
-    // { code: "STSC", name: "Stromness to Scrabster" },
 ];
 
 requestData(month, year, routeCode.value);
